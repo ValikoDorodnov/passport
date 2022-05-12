d60a80d9fde5dda1a9084df150c106779e5560dd
Valentin Dorodnov <valikodorodnov@gmail.com>
2022-05-12 18:52:03 +0300

    Добавил Dockerfile

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
 func main() {
-	println("Hello from GO!")
+	http.HandleFunc("/", HelloServer)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello world")
 }
